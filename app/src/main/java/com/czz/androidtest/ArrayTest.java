99d8713f3a0113835217c1f9fbe3e1c971fb1d1c
zhouyn <932899053@qq.com>
2020-01-09 17:49:22 +0800

    两种直接插入排序对比

--- app/src/main/java/com/czz/androidtest/ArrayTest.java
+++ app/src/main/java/com/czz/androidtest/ArrayTest.java
@@ -20,9 +20,11 @@ public class ArrayTest {
 //            模拟已经排序好的分组
 //            arr[i] = i;
         }
+
         insertSort1(arr);
         insertSort2(arr);
         shellSort(arr);
+
     }
 
     //寻找数组中第二小的元素
@@ -217,32 +219,6 @@ public class ArrayTest {
         return getArrayString(arr);
     }
 
-    //希尔排序
-    public static String shellSort(int[] array) {
-        long start = System.currentTimeMillis();
-        //希尔排序
-        int gap = array.length;
-        while (true) {
-            gap /= 3;   //增量每次减半
-            for (int i = 0; i < gap; i++) {
-                for (int j = i + gap; j < array.length; j += gap) {//这个循环里其实就是一个插入排序
-                    int temp = array[j];
-                    int k = j - gap;
-                    while (k >= 0 && array[k] > temp) {
-                        array[k + gap] = array[k];
-                        k -= gap;
-                    }
-                    array[k + gap] = temp;
-                }
-            }
-            if (gap == 1)
-                break;
-        }
-        long end = System.currentTimeMillis();
-        System.out.println("希尔排序排序用时==" + (end - start));
-        return getArrayString(array);
-    }
-
 
     //归并排序
     public static String arrayNight() {
@@ -304,38 +280,42 @@ public class ArrayTest {
      */
 
     /**
+     * 最坏和平均时间复杂度为n²，数组长度较小时效率较高；数组本身有序的时候时间复杂度为n。
      * 取出已排序的元素序列后边第一个未排序元素（下标记为j），用临时变量保存起来
      * 通过比较待排序元素与arr[j-1]大小，以arr[j]=arr[j-1]的方式，将已排序的其他元素向后移动一位
      * 最后找到合适位置将临时变量保存的未排序元素保存起来
      */
-    public static String insertSort1(int[] arr) {
+    public static String insertSort1(int[] array) {
+        int[] arr = array.clone();
         long start = System.currentTimeMillis();
         for (int i = 1; i < arr.length; i++) {  //执行次数：n
             int tem = arr[i];   //执行次数：n
-            int j = i;  //执行次数：n
+            int j = i - 1;  //执行次数：n
 
             //下方tem < arr[j - 1]易出错，容易写成arr[j]< arr[j - 1]
-            while (j > 0 && tem < arr[j - 1]) { //执行次数：最大n 最小1
-                arr[j] = arr[j - 1];    //执行次数：最大n 最小1
+            while (j >= 0 && tem < arr[j]) { //执行次数：最大n 最小1
+                arr[j + 1] = arr[j];    //执行次数：最大n 最小1
                 j--;    //执行次数：最大n 最小1
             }
 
-            arr[j] = tem;//执行次数：n
+            arr[j + 1] = tem;//执行次数：n
 
         }
         long end = System.currentTimeMillis();
         System.out.println("insertSort1排序用时==" + (end - start));
         //执行次数：最高：n+n+n+n*(n+n+n)+n=12n²  最低：n+n+n+n*(1+1+1)+n=12n
-        return getArrayString(arr);
+
+        String string = getArrayString(arr);
+//        System.out.println(string);
+        return string;
     }
 
     /**
-     * 借鉴冒泡排序
+     * 借鉴冒泡排序，此直接插入方式没insertSort1好，知道即可。
      * 内层for循环是对已排序好的再加一个当前正在找位置的元素组成的序列的逆序遍历,从最后一位开始跟前一个元素比较，若比前一位小，就交换
      */
-    public static String insertSort2(int[] arr) {
-
-
+    public static String insertSort2(int[] array) {
+        int[] arr = array.clone();
         long start = System.currentTimeMillis();
         for (int i = 0; i < arr.length - 1; i++) {  //执行次数：n
             for (int j = i + 1; j > 0; j--) {   //执行次数：n
@@ -352,7 +332,65 @@ public class ArrayTest {
         }
         long end = System.currentTimeMillis();
         System.out.println("insertSort2排序用时==" + (end - start));
+//        String string = getArrayString(arr);
+//        System.out.println(string);
         return getArrayString(arr);
+    }
+
+
+    //希尔排序
+
+    /**
+     * 类型：是一种非稳定的插入排序
+     * 原理：先说直接插入排序的两个特性
+     * 1:因为直接插入排序平均时间复杂度为n²，所以在数组个数很少的时候比个数很多的时候要快；
+     * 2:直接插入排序在数组本身有序的情况下时间复杂度为n
+     * 为什么希尔排序比直接插入排序更快呢？因为希尔排序利用这两点做了以下操作：
+     * 定义一个合适的递减的增量，按照增量将数组分成几个分组分别进行直接插入排序。一开始增量较大，分成的组数也多，每一组的元素很少，根据插入排序特点一可知：
+     * 数组个数很少的时候比个数很多的时候要快，所以会很快。
+     * 之后随着增量的递减，分组的数量会越来越少，数组也渐渐变得有序，最后增量为1的时候，对整个数组进行直接插入排序，由插入排序特点二可知：
+     * 直接插入排序在数组本身有序的情况下时间复杂度为n，所以也很快。
+     */
+    public static String shellSort(int[] arr) {
+        int[] array = arr.clone();
+        long start = System.currentTimeMillis();
+        //gap就是间隔，就是用来将每相差gap个下标的元素各个元素提出来单独插入排序。
+
+        int gap = array.length / 2;
+        for (; gap >= 1; gap /= 2) { //操作次数 ：log2n
+
+            for (int i = 0; i < gap; i++) {
+                //这个循环我让懵逼了半天，一开始没有写这个循环，但排序结果也是正确的，但是从希尔的逻辑上是讲不通的，后来突然想到，不管前边gap值为几
+                //最后都会为1，进行一次整体的直接插入排序，所以结果也是正确的，只是速度变慢了，下面注释掉的是我说的这种错误的写法。
+
+                for (int j = i + gap; j < array.length; j += gap) { //操作次数 ：n/gap
+                    //这个循环里其实就是一个插入排序
+                    int temp = array[j];
+                    int k = j - gap;
+                    while (k >= 0 && temp < array[k]) { //操作次数 ：最小1 最大n/gap
+                        array[k + gap] = array[k];
+                        k -= gap;
+                    }
+                    array[k + gap] = temp;
+                }
+            }
+
+//            for (int j = gap; j < array.length; j += gap) { //操作次数 ：n/gap
+//                //这个循环里其实就是一个插入排序
+//                int temp = array[j];
+//                int k = j - gap;
+//                while (k >= 0 && temp < array[k]) { //操作次数 ：最小1 最大n/gap
+//                    array[k + gap] = array[k];
+//                    k -= gap;
+//                }
+//                array[k + gap] = temp;
+//            }
+        }
+        long end = System.currentTimeMillis();
+        System.out.println("希尔排序排序用时==" + (end - start));
+        String string = getArrayString(array);
+//        System.out.println(string);
+        return getArrayString(array);
     }
 
 
